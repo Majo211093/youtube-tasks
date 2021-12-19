6c54465cc919e1ae0d52bd21a7267df2714e6449
Majo211093 <96183066+Majo211093@users.noreply.github.com>
2021-12-19 10:35:48 -0300

    Cambio de la visualización de la gráfica
    Cambié el gráfico a un gráfico de barras, ordené los valores de trending_date para  eliminar el problema con la primera fecha. ETC

--- grafica.py
+++ grafica.py
@@ -11,21 +11,23 @@ import datetime as dt
 from datetime import time
 
 
-plt.figure()
+plt.figure(figsize=(19, 13))
 USvideos = pd.read_csv('D:/Auxiliar/Descargas/proyectosPy/USvideos.csv')
-likes_por_fechas_Child = USvideos[USvideos['video_id'] == 'VYOjWnS4cMY'].groupby(['trending_date'])
-likes_por_fechas_Child['likes'].sum().plot(color='b', label= 'Likes')
-plt.semilogy()
 
 vistas_por_fechas_Child = USvideos[USvideos['video_id'] == 'VYOjWnS4cMY'].groupby(['trending_date'])
-vistas_por_fechas_Child['views'].sum().plot(color='y', label= 'Views')
+vistas_por_fechas_Child['views'].sum().sort_values().plot(logy=True, color='y', label= 'Views')
+
+likes_por_fechas_Child = USvideos[USvideos['video_id'] == 'VYOjWnS4cMY'].groupby(['trending_date'])
+likes_por_fechas_Child['likes'].sum(axis=1).sort_values().plot.bar(color='b', label= 'Likes')
+#plt.semilogy()
 
 dislikes_por_fechas_Child = USvideos[USvideos['video_id'] == 'VYOjWnS4cMY'].groupby(['trending_date'])
-dislikes_por_fechas_Child['dislikes'].sum().plot(color='r', label = 'Dislikes')
+dislikes_por_fechas_Child['dislikes'].sum(axis=1).sort_values().plot.bar(color='r', label = 'Dislikes')
 plt.xlabel('Trending Date')
 
 plt.title('Childish Gambino - This is America (Official Video)')  
 plt.legend()
 
+#normalizar los tres valores o aplicar suma acumulativa
    
 
